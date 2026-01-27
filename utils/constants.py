e9bd7f6ac36cd27ff37cadb1c29ff09dc6855336
Abdelrahman Mostafa <7abdelrahmanmostafa7@gmail.com>
2026-01-27 14:20:02 +0300

    Add utils/constants.py with global physics, race, and car constants

---
+++ utils/constants.py
@@ -0,0 +1,17 @@
+#constants.py
+
+"""
+Global constants for simulation
+"""
+
+# Physical constants
+GRAVITY = 9.81        # m/s^2
+AIR_DENSITY = 1.225   # kg/m^3 at sea level
+
+# Race constants
+PIT_STOP_TIME = 22.0  # seconds
+LAP_DISTANCE = 5300.0 # meters (example track length)
+
+# Car constants
+MAX_FUEL = 110.0      # kg (F1 fuel limit)
+MAX_GRIP = 1.5        # arbitrary grip coefficient
